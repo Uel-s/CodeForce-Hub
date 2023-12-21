a3ef451da15640f7e40e1fffb9024da32c2abd3f
Uel-5 <kariukiwaweru8156@gmail.com>
2023-12-21 18:48:19 +0300

    added key.id

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./index.css";
 import React, { useState } from "react";
 import Employee from "./components/Employee";
+import {v4 as uuidv4} from "uuid" 
 
 function App() {
   const [role, setRole] = useState("Junior dev");
@@ -27,59 +28,21 @@ function App() {
     <div className="App">
       {showEmployees ? (
         <>
+          <input type="text" onChange={(e) => setRole(e.target.value)} />
+
           <div className="flex flex-wrap justify-center">
-            <input type="text" onChange={(e) => setRole(e.target.value)} />
-            <Employee
-              name="Chris"
-              role=""
-              img="image1.jpeg"
-            />
-            <Employee
-              name="Eninem"
-              role="Backend Developer"
-              img="image2.jpeg"
-            />
-            <Employee name="Anne" role={role} img="image3.jpeg" />
-            <Employee name="Doja" role="DevOps Engineer" img="image4.jpeg" />
-            <Employee name="Eva" role="UI/UX Designer" img="image5.jpeg" />
-            <Employee
-              name="Ndumba"
-              role="Software Architect"
-              img="image6.jpeg"
-            />
-            <Employee name="Griffin" role="QA Engineer" img="image7.jpeg" />
-            <Employee
-              name="Hank"
-              role="Database Administrator"
-              img="image8.jpeg"
-            />
-            <Employee
-              name="Lee"
-              role="Mobile App Developer"
-              img="image9.jpeg"
-            />
-            <Employee name="Jack" role="Cloud Engineer" img="image10.jpeg" />
-            <Employee
-              name="Kratos"
-              role="Security Analyst"
-              img="image11.jpeg"
-            />
-            <Employee
-              name="Leo"
-              role="Machine Learning Engineer"
-              img="image12.jpeg"
-            />
-            <Employee name="Octavia" role="Data Scientist" img="image13.jpeg" />
-            <Employee
-              name="Nathan"
-              role="Embedded Systems Engineer"
-              img="image14.jpeg"
-            />
-            <Employee
-              name="Trin"
-              role="Software Engineer in Test"
-              img="image15.jpeg"
-            />
+            {employees.map((employee) => {
+              console.log(uuidv4);
+              return (
+                <Employee
+                key = {uuidv4()}
+                name={employee.name}
+                role={employee.role}
+                img={employee.img}
+              />
+              );
+              
+            })}
           </div>
         </>
       ) : (
