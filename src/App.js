27f1069f22f50b4f50b2fd3ec533ecae1192a792
Uel-5 <kariukiwaweru8156@gmail.com>
2023-12-21 05:59:15 +0300

    added new usestate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import Employee from "./components/Employee";
 
 function App() {
   const [role, setRole] = useState("Junior dev");
+  const [employees, setEmployees] = useState([
+    { name: "Chris", role: "Frontend Developer", img: "image1.jpeg" },
+    { name: "Eninem", role: "Backend Developer", img: "image2.jpeg" },
+    { name: "Anne", role: "Junior Dev", img: "image3.jpeg" },
+    { name: "Doja", role: "DevOps", img: "image4.jpeg" },
+    { name: "Eva", role: "UI/UX Designer", img: "image5.jpeg" },
+    { name: "Ndumba", role: "Software Architect", img: "image6.jpeg" },
+    { name: "Griffin", role: "QA Enginner", img: "image7.jpeg" },
+    { name: "Hank", role: "Database Administrator", img: "image8.jpeg" },
+    { name: "Lee", role: "Mobile App Dev", img: "image9.jpeg" },
+    { name: "Jack", role: "Cloud Enginner", img: "image10.jpeg" },
+    { name: "Krators", role: "Security Analyst", img: "image11.jpeg" },
+    { name: "Leo", role: "Machine Learning Engineer", img: "image12.jpeg" },
+    { name: "Octavia", role: "Data Scientist", img: "image13.jpeg" },
+    { name: "Nathan", role: "Embedded Systems Engineer", img: "image14.jpeg" },
+    { name: "Trin", role: "Software Engineer in Test", img: "image15.jpeg" },
+  ]);
   const showEmployees = true;
 
   return (
@@ -14,7 +31,7 @@ function App() {
             <input type="text" onChange={(e) => setRole(e.target.value)} />
             <Employee
               name="Chris"
-              role="Frontend Developer"
+              role=""
               img="image1.jpeg"
             />
             <Employee
