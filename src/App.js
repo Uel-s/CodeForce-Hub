e403a294fdfa4c7fb75b38a189a72f0b1d5d74bf
Uel-5 <kariukiwaweru8156@gmail.com>
2023-12-22 21:52:08 +0300

    added update function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import "./index.css";
 import React, { useState } from "react";
 import Employee from "./components/Employee";
-import {v4 as uuidv4} from "uuid" 
+//import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("Junior dev");
   const [employees, setEmployees] = useState([
-    { name: "Chris", role: "Frontend Developer", img: "image1.jpeg" },
-    { name: "Eninem", role: "Backend Developer", img: "image2.jpeg" },
-    { name: "Anne", role: "Junior Dev", img: "image3.jpeg" },
-    { name: "Doja", role: "DevOps", img: "image4.jpeg" },
-    { name: "Eva", role: "UI/UX Designer", img: "image5.jpeg" },
-    { name: "Ndumba", role: "Software Architect", img: "image6.jpeg" },
-    { name: "Griffin", role: "QA Enginner", img: "image7.jpeg" },
-    { name: "Hank", role: "Database Administrator", img: "image8.jpeg" },
-    { name: "Lee", role: "Mobile App Dev", img: "image9.jpeg" },
-    { name: "Jack", role: "Cloud Enginner", img: "image10.jpeg" },
-    { name: "Krators", role: "Security Analyst", img: "image11.jpeg" },
-    { name: "Leo", role: "Machine Learning Engineer", img: "image12.jpeg" },
-    { name: "Octavia", role: "Data Scientist", img: "image13.jpeg" },
-    { name: "Nathan", role: "Embedded Systems Engineer", img: "image14.jpeg" },
-    { name: "Trin", role: "Software Engineer in Test", img: "image15.jpeg" },
+    { id: 1, name: "Chris", role: "Frontend Developer", img: "image1.jpeg" },
+    { id: 2, name: "Eninem", role: "Backend Developer", img: "image2.jpeg" },
+    { id: 3, name: "Anne", role: "Junior Dev", img: "image3.jpeg" },
+    { id: 4, name: "Doja", role: "DevOps", img: "image4.jpeg" },
+    { id: 5, name: "Eva", role: "UI/UX Designer", img: "image5.jpeg" },
+    { id: 6, name: "Ndumba", role: "Software Architect", img: "image6.jpeg" },
+    { id: 7, name: "Griffin", role: "QA Enginner", img: "image7.jpeg" },
+    { id: 8, name: "Hank", role: "Database Administrator", img: "image8.jpeg" },
+    { id: 9, name: "Lee", role: "Mobile App Dev", img: "image9.jpeg" },
+    { id: 10, name: "Jack", role: "Cloud Enginner", img: "image10.jpeg" },
+    { id: 11, name: "Krators", role: "Security Analyst", img: "image11.jpeg" },
+    {
+      id: 12,
+      name: "Leo",
+      role: "Machine Learning Engineer",
+      img: "image12.jpeg",
+    },
+    { id: 13, name: "Octavia", role: "Data Scientist", img: "image13.jpeg" },
+    {
+      id: 14,
+      name: "Nathan",
+      role: "Embedded Systems Engineer",
+      img: "image14.jpeg",
+    },
+    {
+      id: 15,
+      name: "Trin",
+      role: "Software Engineer in Test",
+      img: "image15.jpeg",
+    },
   ]);
   const showEmployees = true;
+
+  function updateEmployee(id, newName, newRole) {
+    const updateEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        return{...employee, name:newName, role:newRole};
+      }
+      return employee;
+    });
+    setEmployees(updateEmployees);
+  }
 
   return (
     <div className="App">
@@ -34,13 +59,14 @@ function App() {
             {employees.map((employee) => {
               return (
                 <Employee
-                key = {uuidv4()}
-                name={employee.name}
-                role={employee.role}
-                img={employee.img}
-              />
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  updateEmployee={updateEmployee}
+                />
               );
-              
             })}
           </div>
         </>
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
+//import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function EditEmployee() {
+function EditEmployee(props) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
 
   return (
     <>
@@ -27,7 +29,14 @@ function EditEmployee() {
           <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form id="editmodal" className="w-full max-w-sm">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              props.updateEmployee(props.id, name, role);
+            }}
+            id="editmodal"
+            className="w-full max-w-sm"
+          >
             <div className="md:flex md:items-center mb-6">
               <div className="md:w-1/3">
                 <label
@@ -42,7 +51,8 @@ function EditEmployee() {
                   className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="name"
                   type="text"
-                  value="Jane Doe"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
             </div>
@@ -60,7 +70,8 @@ function EditEmployee() {
                   className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="role"
                   type="text"
-                  value="Jane Doe"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
                 />
               </div>
             </div>
@@ -80,6 +91,7 @@ function EditEmployee() {
           <button
             className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
             form="editmodal"
+            onClick={handleClose}
           >
             Update
           </button>
